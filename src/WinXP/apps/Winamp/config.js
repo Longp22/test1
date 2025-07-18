adc93c599af52e59dc58c48013f9b69c6ddaa6a5
AsLongp22vn <aslongp22vn1@gmail.com>
2025-07-18 17:10:01 +0700

    fix

--- src/WinXP/apps/Winamp/config.js
+++ src/WinXP/apps/Winamp/config.js
@@ -1,86 +1,118 @@
 // from Webamp demo
 
-const album = 'netBloc Vol. 24: tiuqottigeloot';
+const album = "netBloc Vol. 24: tiuqottigeloot";
 
 export const initialTracks = [
   {
-    url: 'https://ia601807.us.archive.org/1/items/trinity-encore-visualizer-flashing-lights-warning/tn-shi%20-%20Synthesis..mp3',
+    url: "https://ia601807.us.archive.org/1/items/trinity-encore-visualizer-flashing-lights-warning/tn-shi%20-%20Synthesis..mp3",
     duration: 0,
-    metaData: { title: 'Synthesis.', artist: 'tn-shi', album: '' },
+    metaData: { title: "Synthesis.", artist: "tn-shi", album: "" },
   },
   {
-    url: 'https://ia601807.us.archive.org/1/items/trinity-encore-visualizer-flashing-lights-warning/DECO%EF%BC%8A27%20-%20%E3%83%8F%E3%83%AD%E3%83%BC%E3%82%BB%E3%82%AB%E3%82%A4%20feat.%20%E5%88%9D%E9%9F%B3%E3%83%9F%E3%82%AF.mp3',
+    url: "https://ia601807.us.archive.org/1/items/trinity-encore-visualizer-flashing-lights-warning/DECO%EF%BC%8A27%20-%20%E3%83%8F%E3%83%AD%E3%83%BC%E3%82%BB%E3%82%AB%E3%82%A4%20feat.%20%E5%88%9D%E9%9F%B3%E3%83%9F%E3%82%AF.mp3",
     duration: 0,
-    metaData: { title: 'ハローセカイ', artist: 'DECO*27 feat. 初音ミク', album: '' },
+    metaData: {
+      title: "ハローセカイ",
+      artist: "DECO*27 feat. 初音ミク",
+      album: "",
+    },
   },
   {
-    url: 'https://ia601807.us.archive.org/1/items/trinity-encore-visualizer-flashing-lights-warning/DECO%EF%BC%8A27%20-%20%E3%83%A2%E3%83%8B%E3%82%BF%E3%83%AA%E3%83%B3%E3%82%B0%20feat.%20%E5%88%9D%E9%9F%B3%E3%83%9F%E3%82%AF.mp3',
+    url: "https://ia601807.us.archive.org/1/items/trinity-encore-visualizer-flashing-lights-warning/DECO%EF%BC%8A27%20-%20%E3%83%A2%E3%83%8B%E3%82%BF%E3%83%AA%E3%83%B3%E3%82%B0%20feat.%20%E5%88%9D%E9%9F%B3%E3%83%9F%E3%82%AF.mp3",
     duration: 0,
-    metaData: { title: 'モニタリング', artist: 'DECO*27 feat. 初音ミク', album: '' },
+    metaData: {
+      title: "モニタリング",
+      artist: "DECO*27 feat. 初音ミク",
+      album: "",
+    },
   },
   {
-    url: 'https://ia601807.us.archive.org/1/items/trinity-encore-visualizer-flashing-lights-warning/Fukashigi%20no%20KARTE.mp3',
+    url: "https://ia601807.us.archive.org/1/items/trinity-encore-visualizer-flashing-lights-warning/Fukashigi%20no%20KARTE.mp3",
     duration: 0,
-    metaData: { title: 'Fukashigi no KARTE', artist: '', album: '' },
+    metaData: { title: "Fukashigi no KARTE", artist: "", album: "" },
   },
   {
-    url: 'https://ia601807.us.archive.org/1/items/trinity-encore-visualizer-flashing-lights-warning/Kikuo%20-%20%E6%84%9B%E3%81%97%E3%81%A6%E6%84%9B%E3%81%97%E3%81%A6%E6%84%9B%E3%81%97%E3%81%A6.mp3',
+    url: "https://ia601807.us.archive.org/1/items/trinity-encore-visualizer-flashing-lights-warning/Kikuo%20-%20%E6%84%9B%E3%81%97%E3%81%A6%E6%84%9B%E3%81%97%E3%81%A6%E6%84%9B%E3%81%97%E3%81%A6.mp3",
     duration: 0,
-    metaData: { title: '愛して愛して愛して', artist: 'Kikuo', album: '' },
+    metaData: { title: "愛して愛して愛して", artist: "Kikuo", album: "" },
   },
   {
-    url: 'https://ia601807.us.archive.org/1/items/trinity-encore-visualizer-flashing-lights-warning/Trinity%20Encore%20-%20Visualizer%20%7BFLASHING%20LIGHTS%20WARNING%7D.mp3',
+    url: "https://ia601807.us.archive.org/1/items/trinity-encore-visualizer-flashing-lights-warning/Trinity%20Encore%20-%20Visualizer%20%7BFLASHING%20LIGHTS%20WARNING%7D.mp3",
     duration: 0,
-    metaData: { title: 'Visualizer {FLASHING LIGHTS WARNING}', artist: 'Trinity Encore', album: '' },
+    metaData: {
+      title: "Visualizer {FLASHING LIGHTS WARNING}",
+      artist: "Trinity Encore",
+      album: "",
+    },
   },
   {
-    url: 'https://ia601807.us.archive.org/1/items/trinity-encore-visualizer-flashing-lights-warning/snooze%20%E2%A7%B8%20wotaku%20feat.%20SHIKI.mp3',
+    url: "https://ia601807.us.archive.org/1/items/trinity-encore-visualizer-flashing-lights-warning/snooze%20%E2%A7%B8%20wotaku%20feat.%20SHIKI.mp3",
     duration: 0,
-    metaData: { title: 'snooze', artist: 'wotaku feat. SHIKI', album: '' },
+    metaData: { title: "snooze", artist: "wotaku feat. SHIKI", album: "" },
   },
   {
-    url: 'https://ia601807.us.archive.org/1/items/trinity-encore-visualizer-flashing-lights-warning/%E3%80%90Kanaria%E3%80%91BRAIN.mp3',
+    url: "https://ia601807.us.archive.org/1/items/trinity-encore-visualizer-flashing-lights-warning/%E3%80%90Kanaria%E3%80%91BRAIN.mp3",
     duration: 0,
-    metaData: { title: 'BRAIN', artist: 'Kanaria', album: '' },
+    metaData: { title: "BRAIN", artist: "Kanaria", album: "" },
   },
   {
-    url: 'https://ia601807.us.archive.org/1/items/trinity-encore-visualizer-flashing-lights-warning/%E3%81%8D%E3%82%85%E3%81%86%E3%81%8F%E3%82%89%E3%82%8A%E3%82%93%20%E2%A7%B8%20%E3%81%84%E3%82%88%E3%82%8F%20feat.%E5%8F%AF%E4%B8%8D%EF%BC%88Kyu-kurarin%20%E2%A7%B8%20Iyowa%20feat.Kafu%EF%BC%89.mp3',
+    url: "https://ia601807.us.archive.org/1/items/trinity-encore-visualizer-flashing-lights-warning/%E3%81%8D%E3%82%85%E3%81%86%E3%81%8F%E3%82%89%E3%82%8A%E3%82%93%20%E2%A7%B8%20%E3%81%84%E3%82%88%E3%82%8F%20feat.%E5%8F%AF%E4%B8%8D%EF%BC%88Kyu-kurarin%20%E2%A7%B8%20Iyowa%20feat.Kafu%EF%BC%89.mp3",
     duration: 0,
-    metaData: { title: 'きゅうくらりん', artist: 'いよわ feat. 可不', album: '' },
+    metaData: {
+      title: "きゅうくらりん",
+      artist: "いよわ feat. 可不",
+      album: "",
+    },
   },
   {
-    url: 'https://ia601807.us.archive.org/1/items/trinity-encore-visualizer-flashing-lights-warning/%E3%83%88%E3%83%AA%E3%82%B3%E3%83%AD%E3%83%BC%E3%82%B8%E3%83%A5%E2%A7%B825%E6%99%82%E3%80%81%E3%83%8A%E3%82%A4%E3%83%88%E3%82%B3%E3%83%BC%E3%83%89%E3%81%A7%E3%80%82%20%C3%97%20%E5%88%9D%E9%9F%B3%E3%83%9F%E3%82%AF.mp3',
+    url: "https://ia601807.us.archive.org/1/items/trinity-encore-visualizer-flashing-lights-warning/%E3%83%88%E3%83%AA%E3%82%B3%E3%83%AD%E3%83%BC%E3%82%B8%E3%83%A5%E2%A7%B825%E6%99%82%E3%80%81%E3%83%8A%E3%82%A4%E3%83%88%E3%82%B3%E3%83%BC%E3%83%89%E3%81%A7%E3%80%82%20%C3%97%20%E5%88%9D%E9%9F%B3%E3%83%9F%E3%82%AF.mp3",
     duration: 0,
-    metaData: { title: 'トリコロージュ×25時、ナイトコードで。', artist: '初音ミク', album: '' },
+    metaData: {
+      title: "トリコロージュ×25時、ナイトコードで。",
+      artist: "初音ミク",
+      album: "",
+    },
   },
   {
-    url: 'https://ia601807.us.archive.org/1/items/trinity-encore-visualizer-flashing-lights-warning/%E3%83%94%E3%83%8E%E3%82%AD%E3%82%AA%E3%83%94%E3%83%BC%20-%20%E3%83%8E%E3%83%B3%E3%83%96%E3%83%AC%E3%82%B9%E3%83%BB%E3%82%AA%E3%83%96%E3%83%AA%E3%83%BC%E3%82%B8%E3%83%A5%20feat.%20%E5%88%9D%E9%9F%B3%E3%83%9F%E3%82%AF%20%E2%A7%B8%20Non-breath%20oblige.mp3',
+    url: "https://ia601807.us.archive.org/1/items/trinity-encore-visualizer-flashing-lights-warning/%E3%83%94%E3%83%8E%E3%82%AD%E3%82%AA%E3%83%94%E3%83%BC%20-%20%E3%83%8E%E3%83%B3%E3%83%96%E3%83%AC%E3%82%B9%E3%83%BB%E3%82%AA%E3%83%96%E3%83%AA%E3%83%BC%E3%82%B8%E3%83%A5%20feat.%20%E5%88%9D%E9%9F%B3%E3%83%9F%E3%82%AF%20%E2%A7%B8%20Non-breath%20oblige.mp3",
     duration: 0,
-    metaData: { title: 'ノンブレス・オブリージュ', artist: 'ピノキオピー feat. 初音ミク', album: '' },
+    metaData: {
+      title: "ノンブレス・オブリージュ",
+      artist: "ピノキオピー feat. 初音ミク",
+      album: "",
+    },
   },
   {
-    url: 'https://ia601807.us.archive.org/1/items/trinity-encore-visualizer-flashing-lights-warning/%E3%83%95%E3%82%A9%E3%83%8B%E3%82%A4%20%E2%A7%B8%20phony%20-%20kafu%20%5B%E3%82%AA%E3%83%AA%E3%82%B8%E3%83%8A%E3%83%AB%5D.mp3',
+    url: "https://ia601807.us.archive.org/1/items/trinity-encore-visualizer-flashing-lights-warning/%E3%83%95%E3%82%A9%E3%83%8B%E3%82%A4%20%E2%A7%B8%20phony%20-%20kafu%20%5B%E3%82%AA%E3%83%AA%E3%82%B8%E3%83%8A%E3%83%AB%5D.mp3",
     duration: 0,
-    metaData: { title: 'フォニイ', artist: 'phony - kafu', album: '' },
+    metaData: { title: "フォニイ", artist: "phony - kafu", album: "" },
   },
   {
-    url: 'https://ia601807.us.archive.org/1/items/trinity-encore-visualizer-flashing-lights-warning/%E6%9F%8A%E3%83%9E%E3%82%B0%E3%83%8D%E3%82%BF%E3%82%A4%E3%83%88%20-%20%E3%83%9E%E3%83%BC%E3%82%B7%E3%83%A3%E3%83%AB%EF%BC%8E%E3%83%9E%E3%82%AD%E3%82%B7%E3%83%9E%E3%82%A4%E3%82%B6%E3%83%BC%20%E2%A7%B8%20%E5%8F%AF%E4%B8%8D.mp3',
+    url: "https://ia601807.us.archive.org/1/items/trinity-encore-visualizer-flashing-lights-warning/%E6%9F%8A%E3%83%9E%E3%82%B0%E3%83%8D%E3%82%BF%E3%82%A4%E3%83%88%20-%20%E3%83%9E%E3%83%BC%E3%82%B7%E3%83%A3%E3%83%AB%EF%BC%8E%E3%83%9E%E3%82%AD%E3%82%B7%E3%83%9E%E3%82%A4%E3%82%B6%E3%83%BC%20%E2%A7%B8%20%E5%8F%AF%E4%B8%8D.mp3",
     duration: 0,
-    metaData: { title: 'マーシャル・マキシマイザー', artist: '柊マグネタイト × 可不', album: '' },
+    metaData: {
+      title: "マーシャル・マキシマイザー",
+      artist: "柊マグネタイト × 可不",
+      album: "",
+    },
   },
   {
-    url: 'https://ia601807.us.archive.org/1/items/trinity-encore-visualizer-flashing-lights-warning/%E7%86%B1%E7%95%B0%E5%B8%B8%20%28feat.%20%E5%AE%B5%E5%B4%8E%E5%A5%8F%20%26%20%E6%9C%9D%E6%AF%94%E5%A5%88%E3%81%BE%E3%81%B5%E3%82%86%20%26%20%E6%9D%B1%E9%9B%B2%E7%B5%B5%E5%90%8D%20%26%20%E6%9A%81%E5%B1%B1%E7%91%9E%E5%B8%8C%20%26%20KAITO%29.mp3',
+    url: "https://ia601807.us.archive.org/1/items/trinity-encore-visualizer-flashing-lights-warning/%E7%86%B1%E7%95%B0%E5%B8%B8%20%28feat.%20%E5%AE%B5%E5%B4%8E%E5%A5%8F%20%26%20%E6%9C%9D%E6%AF%94%E5%A5%88%E3%81%BE%E3%81%B5%E3%82%86%20%26%20%E6%9D%B1%E9%9B%B2%E7%B5%B5%E5%90%8D%20%26%20%E6%9A%81%E5%B1%B1%E7%91%9E%E5%B8%8C%20%26%20KAITO%29.mp3",
     duration: 0,
-    metaData: { title: '熱異常', artist: '奏崎奏 & 朝比奈まふゆ & 東雲絵名 & 暁山瑞希 & KAITO', album: '' },
+    metaData: {
+      title: "熱異常",
+      artist: "奏崎奏 & 朝比奈まふゆ & 東雲絵名 & 暁山瑞希 & KAITO",
+      album: "",
+    },
   },
   {
-    url: 'https://ia601807.us.archive.org/1/items/trinity-encore-visualizer-flashing-lights-warning/%E7%A8%B2%E8%91%89%E6%9B%87%E3%80%8E%E3%83%AA%E3%83%AC%E3%82%A4%E3%82%A2%E3%82%A6%E3%82%BF%E3%83%BC%E3%80%8FVo.%20%E6%AD%8C%E6%84%9B%E3%83%A6%E3%82%AD.mp3',
+    url: "https://ia601807.us.archive.org/1/items/trinity-encore-visualizer-flashing-lights-warning/%E7%A8%B2%E8%91%89%E6%9B%87%E3%80%8E%E3%83%AA%E3%83%AC%E3%82%A4%E3%82%A2%E3%82%A6%E3%82%BF%E3%83%BC%E3%80%8FVo.%20%E6%AD%8C%E6%84%9B%E3%83%A6%E3%82%AD.mp3",
     duration: 0,
-    metaData: { title: 'リレイアウター', artist: '歌愛ユキ', album: '' },
+    metaData: { title: "リレイアウター", artist: "歌愛ユキ", album: "" },
   },
   {
-    url: 'https://ia601807.us.archive.org/1/items/trinity-encore-visualizer-flashing-lights-warning/%E9%9B%91%E9%AD%9A%20%E2%A7%B8%20%E4%BA%9E%E5%8C%97%E3%83%8D%E3%83%AB.mp3',
+    url: "https://ia601807.us.archive.org/1/items/trinity-encore-visualizer-flashing-lights-warning/%E9%9B%91%E9%AD%9A%20%E2%A7%B8%20%E4%BA%9E%E5%8C%97%E3%83%8D%E3%83%AB.mp3",
     duration: 0,
-    metaData: { title: '雑魚', artist: '亞北ネル', album: '' },
+    metaData: { title: "雑魚", artist: "亞北ネル", album: "" },
   },
 ];
